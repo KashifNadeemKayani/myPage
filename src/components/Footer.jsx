1b7e173c0e781c09549b00fb99fb851b088596ff
KashifNadeemKayani <KAYANIKASHIF970@GMAIL.COM>
2024-09-01 15:40:09 +0500

    added padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p>© 2024 The Little Stationer. All rights reserved.</p>
-      <a href="https://wa.me/yourwhatsapplink" className="text-white mx-3" target="_blank" rel="noopener noreferrer">
-      <FontAwesomeIcon icon={faWhatsapp} size="2x" /></a>
+      <a href="https://chat.whatsapp.com/C4fQhFyWbTC0vQkcJqeA7V" className={`text-white mx-3 ${styles.anker}`} target="_blank" rel="noopener noreferrer" >
+      <FontAwesomeIcon icon={faWhatsapp} size="2x"/> &nbsp; Join Whatsapp</a>
     </footer>
   );
 }
